d978ed32d8d66126f864e0b67b86342b3d0f33c2
Robin Gerg <60444879+8Klaro8@users.noreply.github.com>
2022-04-25 19:10:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired, url
 import csv
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'THE KEY'
 Bootstrap(app)
 
 # Creating WTF form
